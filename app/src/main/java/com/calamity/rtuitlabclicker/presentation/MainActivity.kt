74e625a7fe955c8be982d9d0a7ab341711f14a3f
CalamityDeadshot <semyon.pyankov@gmail.com>
2021-09-20 16:33:55 +0300

    Base package structure and a helper class

--- app/src/main/java/com/calamity/rtuitlabclicker/presentation/MainActivity.kt
+++ app/src/main/java/com/calamity/rtuitlabclicker/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.calamity.rtuitlabclicker
+package com.calamity.rtuitlabclicker.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.calamity.rtuitlabclicker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
